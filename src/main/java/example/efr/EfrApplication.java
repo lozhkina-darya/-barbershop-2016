c7aade548f2d674a0ab8245cd46163f80c32e379
Ложкина Дарья Дмитриевна (4038118) <lozhkina@dev.vtb.ru>
2020-12-24 11:51:02 +0300

    Second commit

---
+++ src/main/java/example/efr/EfrApplication.java
@@ -0,0 +1,11 @@
+package example.efr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfrApplication {
+
+	public static void main(String[] args) { SpringApplication.run(EfrApplication.class, args);}
+
+}
---
+++ src/test/java/example/efr/EfrApplicationTests.java
@@ -0,0 +1,13 @@
+package example.efr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
